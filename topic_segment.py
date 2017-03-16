1a7b4733a7a5780f46d124a2b99c6de5de849a03
Yang Xu <innerfirexy@gmail.com>
2017-03-16 16:39:09 -0400

    add seg_textdata

--- topic_segment.py
+++ topic_segment.py
@@ -40,16 +40,72 @@ def conduct_segment(inputlist, config = 'dp.config'):
 
 
 ##
-# test
-def test():
+# make topicId and inTopicId from the result of conduct_segment
+def make_topic_ids(sent_num, bound_ind):
+    """
+    sent_len: the number of sentences as the input of conduct_segment
+    bound_ind: the sentence indice of topic boundaries, returned by conduct_segment
+    return: a dict of two lists, {'topicId': [1,1,1,...], 'inTopicId': [1,2,3,...]}
+    """
+    ids1 = [] # topicId
+    ids2 = [] # inTopicId
+    for i, ind in enumerate(bound_ind):
+        length = ind+1 if i == 0 else ind - bound_ind[i-1]
+        ids1 += [i+1] * length
+        ids2 += list(range(1, length+1))
+    if sent_num > bound_ind[-1] + 1:
+        ids1 += [ids1[-1]+1] * (sent_num - bound_ind[-1] - 1)
+        ids2 += list(range(1, sent_num - bound_ind[-1]))
+    return {'topicId': ids1, 'inTopicId': ids2}
+
+
+##
+# segment text data file: 'data/SWBD_text_db.csv', 'data/BNC_text_db100.csv', & 'data/BNC_text_dbfull.csv'
+def seg_textdata(inputfile, outputfile, config = 'dp.config'):
+    """
+    config: one of ['dp.config', 'cue.config', 'mcsopt.ai.confi']
+    """
+    assert config in ['dp.config', 'cue.config', 'mcsopt.ai.config']
+
+    # read textdata into a pandas dataframe
+    df = pd.read_csv(inputfile)
+
+    # call conduct_segment for each convId
+    for i, cid in enumerate(df.convId.unique()):
+        sentlist = list(df[df.convId == cid].rawWord)
+        res = conduct_segment(inputlist=sentlist, config=config)
+        # print progress
+        sys.stdout.write('\r{0}/{1} convIds segmented'.format(i+1, len(df.convId.unique())))
+        sys.stdout.flush()
+
+
+
+##
+# test conduct_segment
+def test1():
     inputlist = []
     with open('/Users/yangxu/GitHub/bayes-seg/data/books/clinical/000.ref', 'r') as f:
         for line in f:
             inputlist.append(line.strip())
     print(conduct_segment(inputlist))
 
+##
+# test make_topic_ids
+def test2():
+    bInds = [3,7,11,20]
+    sNum = 25
+    res = make_topic_ids(sNum, bInds)
+    print(bInds)
+    print(res)
+    print('length of topicId: {0}'.format(len(res['topicId'])))
+    print('length of inTopicId: {0}'.format(len(res['inTopicId'])))
+
 
 ##
 # main
 if __name__ == '__main__':
-    test()
+    # test1()
+    # test2()
+
+    # segment SWBD
+    seg_textdata(inputfile='data/SWBD_text_db.csv', outputfile='foo', config='dp.config')
